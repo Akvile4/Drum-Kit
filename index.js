a5a2349fbdffb3fdb063d79c110a26f8815eb196
Akvile4 <94171535+Akvile4@users.noreply.github.com>
2023-09-29 14:13:48 +0100

    added animation to buttons

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
         var buttonInnerHTML = this.innerHTML;
 
         makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+
     })
 
 };
@@ -21,6 +24,9 @@ for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
 document.addEventListener("keydown", function (event) {
 
     makeSound(event.key);
+
+    buttonAnimation(event.key);
+
 }
 );
 
@@ -70,4 +76,14 @@ function makeSound(key) {
 
 }
 
+function buttonAnimation(currentKey) {
 
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout( function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+
+}
